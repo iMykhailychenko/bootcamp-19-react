602c799adfca40e35b8016a3f1bb549a8c033f01
imykhailychenko <igor.c.m@ukr.net>
2022-07-18 12:35:03 +0200

    Модуль 1 Заняття 1

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,23 @@ import ReactDOM from 'react-dom/client';
 
 import images from './assets/images.json';
 
+// React без JSX
+// const img = (src) => React.createElement('img', { src, width: "200px" });
+// const wrp = React.createElement('div', null, [img(images[1]), img(images[2])]);
+
+// Рендер за умовою
 // true && 'hello world' -> 'hello world'
 // false && 'hello world' -> false
 // true || 'hello world' -> true
 // false || 'hello world' -> 'hello world'
 // true ? 'hello world' : 'bye world' -> 'hello world'
 // false ? 'hello world' : 'bye world' -> 'bye world'
+
+// React компоненти
 const Img = ({ src, alt }) => {
   return src ? <img width="200px" src={src} alt={alt} /> : 'немає картинки';
 };
 
-// const wrp = React.createElement('div', null, [img(images[1]), img(images[2])]);
 const App = () => (
   <>
     <Img src={images[0]} alt="image" />
